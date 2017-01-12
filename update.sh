c46bc130d1aa24a40510c1134c3abe60d4185458
Arjan van de Ven <arjan@linux.intel.com>
2017-01-12 15:31:01 +0000

    auto-update

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-VERSION="$1"
+VERSION=`curl -s http://ftp.mozilla.org/pub/firefox/releases/ | grep href | cut -f3 -d">" | cut -f1 -d"/" | cut -f1 -d"-" | grep -v "b" | sort -n | tail -1`
+
 
 if [[ -z "${VERSION}" ]]; then
     echo "Please pass the new version first."
