2098afdbe2b6fd258bef87719ba92a74ac311b0d
David Klimesh <david.j.klimesh@intel.com>
2017-02-03 22:52:34 +0000

    Alter update script to check current version.
    If the new version is different then the version we have
    we should update, but we only want to update if they are
    different (I didn't alter the part to find 'newer' versions)
    
    Signed-off-by: David Klimesh <david.j.klimesh@intel.com>

--- update.sh
+++ update.sh
@@ -4,13 +4,20 @@ VERSION=`curl -s http://ftp.mozilla.org/pub/firefox/releases/ | grep href | cut 
 
 
 if [[ -z "${VERSION}" ]]; then
-    echo "Please pass the new version first."
+    echo "Unable to find version upstream."
     exit 1
 fi
 
+#no spaces in version we hope..
+CURRENT_VERSION=$(grep "^Version" firefox.spec | cut -f4 -d" ")
+
+if [[ v"${CURRENT_VERSION}" == v"${VERSION}" ]]; then
+	exit 2
+fi
+
 sed firefox.spec.in -e "s/\#\#VERSION\#\#/${VERSION}/g" > firefox.spec
-make generateupstream || exit 1
+make generateupstream || exit 3
 
 git add firefox.spec Makefile release upstream
 git commit -s -m "Update to ${VERSION}"
-#make bump
+make bump
