5569d1e76035c80ebcc3918b2e160543f411e0a9
Arjan van de Ven <arjan@linux.intel.com>
2017-03-06 20:57:08 +0000

    typo

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=`curl -s http://ftp.mozilla.org/pub/firefox/releases/ | grep href | cut -f3 -d">" | cut -f1 -d"/" | cut -f1 -d"-" | grep -v "b" | sort -n | tail -1`
+VERSION=`curl -s http://ftp.mozilla.org/pub/firefox/releases/ | grep href | grep -v esr | cut -f3 -d">" | cut -f1 -d"/" | cut -f1 -d"-" | grep -v "b" | sort -n | tail -1`
 
 
 if [[ -z "${VERSION}" ]]; then
