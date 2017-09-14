d85c89635d7cad5981eb35b063920abe297e60b3
Arzhan Kinzhalin <arzhan.i.kinzhalin@intel.com>
2017-09-14 04:34:24 +0000

    Implement Clear Trust store: use p11-kit.
    Change install: use system trust store via p11-kit
    
    On installation, replace a local copy of libnssckbi.so with a symlink to
    p11-kit-trust.so (bridges to Clear Linux Trust Store).

--- firefox.sh
+++ firefox.sh
@@ -5,6 +5,10 @@ BASEDIR="/usr/share/firefox-stub"
 SFILE="${BASEDIR}/firefox-${VERSION}.tar.bz2"
 LFILE="${HOME}/firefox/firefox"
 FDIR="${HOME}/firefox"
+# p11-kit provides a "bridge" for libnss to access the trust information
+NSSCKBI="${FDIR}/libnssckbi.so"
+P11KIT_BRIDGE="/usr/lib64/p11-kit-trust.so"
+
 
 if [[ -x "${LFILE}" ]] ; then
     exec "${LFILE}" $*
@@ -22,5 +26,18 @@ fi
 
 cd "${HOME}"
 tar xf "${SFILE}"
+
+if [ -f "$P11KIT_BRIDGE" ]; then
+    # backup the original and replace it with a link
+    if [ -f "$NSSCKBI" ]; then
+        mv "$NSSCKBI" "${NSSCKBI}.orig"
+        (cd $(dirname "$NSSCKBI") && ln -s "$P11KIT_BRIDGE" $(basename "$NSSCKBI"))
+        if [ $? -ne 0 ]; then
+            1>&2 echo "System trust store: failed to create bridge."
+        fi
+    else
+        1>&2 echo System trust store: nss library not found $(basename "$NSSCKBI")
+    fi
+fi
 
 exec "${LFILE}" $*
