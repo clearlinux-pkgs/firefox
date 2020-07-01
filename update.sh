b62643a7efeb11b54753c5d0c844b0b52d3fd7aa
Patrick McCarty <patrick.mccarty@intel.com>
2020-07-01 16:46:13 -0700

    Improve version sorting and error handling in update.sh
    Signed-off-by: Patrick McCarty <patrick.mccarty@intel.com>

--- update.sh
+++ update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-VERSION=`curl -s http://ftp.mozilla.org/pub/firefox/releases/ | grep href | grep -v esr | cut -f3 -d">" | cut -f1 -d"/" | cut -f1 -d"-" | grep -v "b" | sort -n | tail -1`
+set -e
+set -o pipefail
 
+git pull
+VERSION=$(curl -sSf http://ftp.mozilla.org/pub/firefox/releases/ | grep href | cut -f3 -d">" | cut -f1 -d"/" | grep -Ex '[0-9.]+' | sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n | tail -1)
 
 if [[ -z "${VERSION}" ]]; then
     echo "Unable to find version upstream."
@@ -19,7 +22,7 @@ fi
 sed -e "s/##VERSION##/${VERSION}/g; s/##RELEASE##/${CURRENT_RELEASE}/" firefox.spec.in > firefox.spec
 make generateupstream || exit 3
 
+make bumpnogit
 git add firefox.spec Makefile release upstream
 git commit -s -m "Update to ${VERSION}"
-make bump
 make koji
