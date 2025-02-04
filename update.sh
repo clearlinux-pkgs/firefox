e1c5c1c3e35e1c7c65a37e0efc956843e63cc11a
Brett T. Warden <brett.t.warden@intel.com>
2025-02-04 15:11:33 -0800

    Make update.sh try multiple extensions, if necessary

--- update.sh
+++ update.sh
@@ -33,6 +33,23 @@ fi
 
 sed -e "s/##VERSION##/${VERSION}/g; s/##RELEASE##/${CURRENT_RELEASE}/" $PKG.spec.in > $PKG.spec
 
+# Check that the substituted URLs are actually valid. If not, try to fix them.
+for url in $(grep -iE '(URL|SOURCE)[0-9]*\s*:' $PKG.spec | grep -oE 'https://\S+'); do
+	echo Testing url $url
+	if ! curl -sSIf "${url}"; then
+		echo "Unable to fetch ${url}"
+
+		# Strip the extension and try the common ones
+		base=${url%%.tar.*}
+		for ext in .tar.zst .tar.bz2 .tar.gz .tar.xz; do
+			if curl -ssIf "${base}${ext}"; then
+				sed -i "s#${url}#${base}${ext}#" $PKG.spec
+				break
+			fi
+		done
+	fi
+done
+
 make generateupstream || exit 3
 
 make bumpnogit
